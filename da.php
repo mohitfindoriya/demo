7d04629b645f24e67f2bbbcb5cd53b352afe1c94
mohitfindoriya <44721661+mohitfindoriya@users.noreply.github.com>
2018-11-04 12:01:32 +0530

    Create da.php

---
+++ da.php
@@ -0,0 +1,18 @@
+$json = '[
+    {
+        "title": "Professional JavaScript",
+        "author": "Nicholas C. Zakas"
+    },
+    {
+        "title": "JavaScript: The Definitive Guide",
+        "author": "David Flanagan"
+    },
+    {
+        "title": "High Performance JavaScript",
+        "author": "Nicholas C. Zakas"
+    }
+]';
+
+$books = json_decode($json);
+// access property of object in array
+echo $books[1]->title; 
